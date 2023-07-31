adca522601c1735390455b7a6d46374808239d6c
ChienWeiHsiung <kevinhsiung1022@gmail.com>
2023-07-31 14:26:30 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 
-const API_URL = 'https://1cce-36-228-141-223.ngrok-free.app/';
-// const API_URL = 'http://localhost:5000/';
+// const API_URL = 'https://1cce-36-228-141-223.ngrok-free.app/';
+const API_URL = 'http://localhost:5000/';
 export default API_URL;
